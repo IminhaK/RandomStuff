c6aa9ff6d66fa00b03aeb03af61696dbbef34974
bloop428 <justinschrier@gmail.com>
2018-07-17 11:29:47 -0700

    Changed the way stuff is registered

---
+++ src/main/java/net/bloop/randomstuff/Blocks/StuffBlocks.java
@@ -0,0 +1,11 @@
+package net.bloop.randomstuff.Blocks;
+
+import net.bloop.randomstuff.RandomStuff;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
+import net.minecraftforge.fml.common.registry.GameRegistry.ObjectHolder;
+
+@ObjectHolder(RandomStuff.MODID)
+public class StuffBlocks {
+    public static final Block BLOCK_TEST = Blocks.AIR;
+}
---
+++ src/main/java/net/bloop/randomstuff/Items/ItemBlockBase.java
@@ -0,0 +1,15 @@
+package net.bloop.randomstuff.Items;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.client.util.ITooltipFlag;
+import net.minecraft.item.ItemBlock;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class ItemBlockBase extends ItemBlock {
+    public ItemBlockBase(Block block)
+    {
+        super(block);
+    }
+}
