944d2c4c80286432b337f17c4081e1e8baef23d8
bloop428 <justinschrier@gmail.com>
2019-04-07 16:29:34 -0700

    TNT explodes when you pick it up

--- src/main/java/net/bloop/randomstuff/GameEvents.java
+++ src/main/java/net/bloop/randomstuff/GameEvents.java
@@ -1,12 +1,17 @@
 package net.bloop.randomstuff;
 
 import net.minecraft.entity.item.EntityFireworkRocket;
+import net.minecraft.entity.item.EntityItem;
+import net.minecraft.entity.item.EntityTNTPrimed;
+import net.minecraft.init.Blocks;
 import net.minecraft.init.Items;
+import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.nbt.NBTTagList;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraftforge.event.entity.player.EntityItemPickupEvent;
 import net.minecraftforge.event.world.BlockEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
@@ -38,5 +43,19 @@ public class GameEvents {
         stack.setTagInfo("Fireworks", fireworkTag);
 
         world.spawnEntity(new EntityFireworkRocket(world, pos.getX(), pos.getY(), pos.getZ(), stack));
+    }
+
+    @SubscribeEvent
+    public static void pickupTNT(EntityItemPickupEvent event)
+    {
+        World world = event.getItem().getEntityWorld();
+        EntityItem item = event.getItem();
+        if(item.getItem().getItem() == Item.getItemFromBlock(Blocks.TNT))
+        {
+            EntityTNTPrimed tnt = new EntityTNTPrimed(world, item.posX, item.posY, item.posZ, event.getEntityLiving());
+            tnt.setFuse(0);
+
+            world.spawnEntity(tnt);
+        }
     }
 }
